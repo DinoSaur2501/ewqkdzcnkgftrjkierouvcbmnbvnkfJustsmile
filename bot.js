cab36ad31d75574514acbe0f1d4c8e6ce86caeb4
DinoSaur2501 <45693341+DinoSaur2501@users.noreply.github.com>
2018-12-08 05:37:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "#";
 client.on('ready', () => {
     console.log(`~~~~~~~~~~~~~~~~~`);
     console.log(`Logging into Discord`);
